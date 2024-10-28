c694ee7aedd5e9f746e31dc207e73e897ea92a4a
vivekwebm2020 <66646732+vivekwebm2020@users.noreply.github.com>
2024-10-29 03:04:19 +0530

    Update .gitignore and untrack ignored files

---
+++ db.config.sample.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_PORT', '');
+define('DB_USER', '');
+define('DB_HOST', 'localhost');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+?>
--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
+require_once 'db.config.php';
+
 session_start();
-$conn = new mysqli("localhost", "root", "root@vivek@1986", "php_jquery_modal_mgmt", '3307');
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
